3e1e33123f4523df54b0d0e31af571a823985508
fellon-dnb <fellon@ancevt.com>
2024-12-10 01:49:22 +0300

    unTested

--- firstStage/lesson01/LessonOne/src/main/java/com/fellon/Book.java
+++ firstStage/lesson01/LessonOne/src/main/java/com/fellon/Book.java
@@ -1,4 +1,51 @@
 package com.fellon;
 
 public class Book {
+    private String title;
+    private String author;
+    private int year;
+    private boolean isAvailable;
+
+    public Book(String title, String author, int year, boolean isAvailable) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+        this.isAvailable = isAvailable;
+    }
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+        this.year = 0;
+        this.isAvailable = true;
+    }
+
+    public void borrowBook() {
+        if (this.isAvailable) {
+            this.isAvailable = false;
+            System.out.println("Книга :" + title + " - недоступна!");
+        }
+    }
+    public void returnBook() {
+        this.isAvailable = true;
+        System.out.println("Книга: " + title + " - доступна");
+    }
+    public void displayInfo() {
+        System.out.println("Название: " + title);
+        System.out.println("Автор: " + author);
+        System.out.println("Год: " + year);
+        System.out.println("Книга " + (isAvailable ? " - доступна" : " - недоступна"));
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public String getAuthor() {
+        return author;
+    }
+    public int getYear() {
+        return year;
+    }
 }
--- firstStage/lesson01/LessonOne/src/main/java/com/fellon/Library.java
+++ firstStage/lesson01/LessonOne/src/main/java/com/fellon/Library.java
@@ -1,4 +1,33 @@
 package com.fellon;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Library {
-}
+    private ArrayList<Book> books = new ArrayList<>();
+    public void addBook(Book book){
+        books.add(book);
+    }
+       public void printAvalibleBooks(){
+           System.out.println("В наличии:");
+           for(Book book : books) {
+               if (book.isAvailable()){
+                   book.displayInfo();
+               }
+           }
+       }
+    public void findBooksByAuthor(String author) {
+        System.out.println("Книги " + author + ":");
+        for (Book book : books) {
+            if (book.getAuthor().equals(author)) {
+                book.displayInfo();
+            }
+        }
+    }
+    static {
+        Library library = new Library();
+        library.addBook(new Book("Конституция Российской Федерации", "Неизвестен", 1993, true));
+        library.addBook(new Book("Чистый код", "Роберт Мартин", 2008, true));
+        library.addBook(new Book("Грокаем алгоритмы", "Адитья Бхаргава", 2016, true));
+    }
+    }
